651986fb0275524e977e16855424d38f966a92cb
Jeffrey Yang <54513379+jyang19class@users.noreply.github.com>
2021-11-05 02:14:45 -0400

    final

--- app_skeleton.py
+++ app_skeleton.py
@@ -14,39 +14,35 @@ initialize_db(app)
 
 @app.route('/')
 def root():
-    return "Hello World"
+    return 
 
 @app.route('/getall')
 def getAll():
-    all = Note.objects.to_json()
-    return Response(all, status=200)
 
+    return 
 
 @app.route('/get/<noteId>')
 def get(noteId):
-    note = Note.objects.get(noteId=noteId).to_json()
-    return Response(note, mimetype="application/json", status=200)
+    
+    return 
 
 
 @app.route('/add', methods=['POST'])
 def post():
-    body = request.get_json()
-    Note(**body).save()
-    return '', 200
+    
+
+    return 
 
 @app.route('/change/<noteId>', methods=['PUT'])
 def put(noteId):
-    try:
-        body = request.get_json()
-        Note.objects.get(noteId=noteId).update(**body)
-        return '', 200
-    except Note.DoesNotExist:
-        return "Note not found", 404
+    
+
+    return 
 
 @app.route('/change/<noteId>', methods=['DELETE'])
 def delete(noteId):
-    Note.objects.get(noteId=noteId).delete()
-    return '', 200
+    
+    return 
 
 
 app.run()
